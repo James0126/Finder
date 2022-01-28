37746ef31d6693c5a76e22b95cdf4b6c87077d5a
James0126 <james@terra.money>
2022-01-28 16:14:03 +0900

    clean up codes

--- src/containers/token/IBCAmount.tsx
+++ src/containers/token/IBCAmount.tsx
@@ -1,42 +1,23 @@
-import { isDenomIBC, readAmount } from "@terra.kitchen/utils";
+import { readAmount } from "@terra.kitchen/utils";
 import { useIBCTokens } from "../../queries/assets";
+import { useBankBalance } from "../../queries/bank";
 import Amount from "../../components/Amount";
 import { useNetworkName } from "../../contexts/ChainsContext";
-import { useBankBalance } from "../../queries/bank";
 
 const IBCAmount = ({ address, denom }: { address: string; denom: string }) => {
   const network = useNetworkName();
   const { data: ibcWhitelist } = useIBCTokens();
   const { data: balance } = useBankBalance(address);
 
-  if (!ibcWhitelist || !balance) return null;
+  if (!ibcWhitelist) return null;
 
-  const ibcBalance = balance
-    .filter((coin) => isDenomIBC(coin.denom) && coin.denom === denom)
-    .toArray();
   const whitelist = ibcWhitelist[network];
-
-  const ibc = ibcBalance.map((ibc) => {
-    const hash = ibc.denom.replace("ibc/", "");
-    const { symbol, icon } = whitelist[hash];
-    const value = readAmount(ibc.amount.toString(), { comma: true });
-    return { value, symbol, icon };
-  });
-
   const key = denom.replace("ibc/", "");
   const { symbol, icon } = whitelist[key];
+  const ibc = balance?.get(denom);
+  const amount = readAmount(ibc?.amount.toString(), { comma: true });
 
-  return (
-    <>
-      {ibc.length ? (
-        ibc.map(({ value, symbol, icon }) => (
-          <Amount amount={value} denom={symbol} iconUrl={icon} key={symbol} />
-        ))
-      ) : (
-        <Amount amount={"0"} denom={symbol} iconUrl={icon} key={symbol} />
-      )}
-    </>
-  );
+  return <Amount amount={amount} denom={symbol} iconUrl={icon} key={symbol} />;
 };
 
 export default IBCAmount;
