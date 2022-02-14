14051da64cedfdcd0e1e389d028c66254a5719c0
James0126 <james@terra.money>
2022-02-14 13:28:29 +0900

    test for deploy

--- src/worker/txSearch.ts
+++ src/worker/txSearch.ts
@@ -3,17 +3,16 @@ export default () =>
   // eslint-disable-next-line no-restricted-globals
   (self.onmessage = (message) => {
     const { txs, input, style } = message.data;
-    const result = txs.map((tx) => {
-      const raw_log = tx.raw_log;
+    const result = txs.map((tx: any) => {
+      const { raw_log } = tx;
       const classname = input
         ? raw_log.includes(input)
           ? undefined
           : style
         : undefined;
 
-      const data = { ...tx, classname };
-
-      return data;
+      Object.assign(tx, { classname });
+      return tx;
     });
     postMessage(result);
   });
