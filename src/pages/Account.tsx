75f1ee1df850eeb959057348b78e38f4a52d6d61
James0126 <james@terra.money>
2022-02-18 17:50:28 +0900

    Show validator when account is validator

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,11 +1,21 @@
+import { ValAddress } from "@terra-money/terra.js";
+import Flex from "../components/Flex";
+import { useValidator } from "../queries/staking";
 import AddressDetail from "./AddressDetail";
 import s from "./Account.module.scss";
 
-const Account = ({ address }: { address: string }) => (
-  <section>
-    <h1 className={s.title}>Account</h1>
-    <AddressDetail address={address} />
-  </section>
-);
+const Account = ({ address }: { address: string }) => {
+  const validatorAddress = ValAddress.fromAccAddress(address);
+  const { data: validator } = useValidator(validatorAddress);
+  return (
+    <section>
+      <Flex start className={s.title}>
+        <h1 className={s.account}>Account</h1>
+        {validator && <span className={s.validator}>Validator</span>}
+      </Flex>
+      <AddressDetail address={address} />
+    </section>
+  );
+};
 
 export default Account;
